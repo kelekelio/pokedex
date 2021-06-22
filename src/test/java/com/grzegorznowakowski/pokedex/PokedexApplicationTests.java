7102dc74b43f0db80d3343c1c8c10d428a88cd3b
kelekelio <cristal01@gmail.com>
2021-06-22 17:58:50 +0200

    Initial commit

---
+++ src/test/java/com/grzegorznowakowski/pokedex/PokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grzegorznowakowski.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
